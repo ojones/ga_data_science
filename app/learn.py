b2b289e57e5898a598591007797b7ee7b3b33703
oswald jones <oj@oswalds-MacBook-Pro-2.local>
2016-02-17 17:12:57 -0800

    upgraded to python3, fixed classifier

--- app/learn.py
+++ app/learn.py
@@ -1,12 +1,16 @@
 import pickle, json
+from sklearn.externals import joblib
 
-web_classfier_file_path = 'data/classifiers/me_uni_pp_100' 
+# web_classfier_file_path = 'data/classifiers/me_uni_pp_100' 
+web_classfier_file_path = '/Users/oj/Documents/git/ga_data_science/app/data/filename.pkl' 
+
 
 def get_saved_classifier():
-    f = open(web_classfier_file_path, 'rb')
-    data = pickle.load(f)
-    f.close()
-    return data
+    # f = open(web_classfier_file_path, 'rb')
+    # data = pickle.load(f)
+    # f.close()
+    # return data
+    return joblib.load(web_classfier_file_path)
 
 def get_accuracy(choices):
 
@@ -21,14 +25,14 @@ def get_accuracy(choices):
 	return metrics
 
 def get_classifications(tweets):
-	print 'tweets'
-	print tweets
+	print('tweets')
+	print(tweets)
 
-	print 'start retreiving'
+	print('start retreiving')
 	classifier = get_saved_classifier()
 
-	print "classifications: "
+	print("classifications: ")
 	classifications = classifier.predict(tweets).tolist()
-	print classifications
+	print(classifications)
 
 	return classifications
