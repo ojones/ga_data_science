4522843236bcf44549d1a1b7525f77a0eedfae06
oswald jones <oj@oswalds-MacBook-Pro-2.local>
2016-02-19 00:10:28 -0800

    here goes more nothting

---
+++ play/data/classifier_pipline.py
@@ -0,0 +1,43 @@
+import sqlite3
+conn = sqlite3.connect('/Users/oj/Documents/twitter_db/sentiment.db')
+cur = conn.cursor()
+
+def procesed_text(cur, limit):
+    rows = cur.execute("""
+        select processedUrl_UserName_HashTag from second_run limit {limit};
+        """.format(limit=limit))
+    for row in rows:
+        yield row[0]
+
+def sentiment_value(cur, limit):
+    rows = cur.execute("""
+        select score from second_run limit {limit};
+        """.format(limit=limit))
+    for row in rows:
+        yield row[0]
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([('vect', CountVectorizer()),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultinomialNB()),
+])
+
+row_count = 10000
+data = list(procesed_text(cur, row_count))
+target = list(sentiment_value(cur, row_count))
+text_clf = text_clf.fit(data, target)
+
+print(len([x for x in target if x == '0']))
+
+docs_new = ['God is love', 'OpenGL on the GPU is fast', 'i hate this', 'this is good', 'this was terrible']
+predicted = text_clf.predict(docs_new)
+
+for tweet, sentiment in zip(docs_new, predicted):
+    print('%r => %s' % (tweet, sentiment))
+
+import pickle
+with open('web_classifier.p', 'wb') as f:
+    pickle.dump(text_clf, f, protocol=2)
--- play/learn.py
+++ play/learn.py
@@ -2,41 +2,40 @@ import pickle, json, os
 
 
 def get_saved_classifier():
-	# import urllib
-	# import urllib.request
-	# with urllib.request.urlopen(myurl) as f:
-	#     myfile = pickle.load(f)
-	# opener = urllib.URLopener()
-	# myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
-	# print(" ")
-	# print("please work damnit")
-	# print(" ")
-	# myfile = opener.open(myurl)
-    clf = None
+    myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
+    import urllib
+    opener = urllib.URLopener()
+    print(" ")
+    print("please work damnit")
+    print(" ")
+    myfile = opener.open(myurl)
+    clf = pickle.load(myfile)
     return clf
-    # return joblib.load(web_classfier_file_path)
 
 def get_accuracy(choices):
 
-	classifier = get_classifier(choices)
+    classifier = get_classifier(choices)
 
-	metrics = {
-		'accuracy': classifier + ' accuracy'
-		, 'pos_fmeasure': classifier + ' pos_fmeasure'
-		, 'neg_fmeasure': classifier + ' neg_fmeasure'
-	}
+    metrics = {
+        'accuracy': classifier + ' accuracy'
+        , 'pos_fmeasure': classifier + ' pos_fmeasure'
+        , 'neg_fmeasure': classifier + ' neg_fmeasure'
+    }
 
-	return metrics
+    return metrics
 
 def get_classifications(tweets):
-	print('tweets')
-	print(tweets)
+    print('tweets')
+    print(tweets)
 
-	print('start retreiving')
-	classifier = get_saved_classifier()
+    print('start retreiving')
+    classifier = get_saved_classifier()
 
-	print("classifications: ")
-	classifications = classifier.predict(tweets).tolist()
-	print(classifications)
+    print("classifications: ")
+    classifications = classifier.predict(tweets).tolist()
+    print(classifications)
 
-	return classifications
+    return classifications
+
+if __name__ == '__main__':
+    print(get_saved_classifier())
