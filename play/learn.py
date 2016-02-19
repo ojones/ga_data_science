a41ac9790fb24349aac4eba2a2f34d954542f730
oswald jones <oj@oswalds-MacBook-Pro-2.local>
2016-02-18 21:34:42 -0800

    classifier now on s3

--- play/learn.py
+++ play/learn.py
@@ -1,4 +1,9 @@
 import pickle, json, os
+myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
+myfile = None
+import urllib.request
+with urllib.request.urlopen(myurl) as f:
+    myfile = pickle.load(f)
 # from sklearn.externals import joblib
 
 # web_classfier_file_path = 'data/classifiers/me_uni_pp_100' 
@@ -19,14 +24,15 @@ def get_saved_classifier():
     print("1" + str(os.path.isfile(web_classfier_file_path)))
     print("2" + str(os.path.isfile(web_classfier_file_path2)))
     print("3" + str(os.path.isfile(web_classfier_file_path3)))
-    with open(web_classfier_file_path, 'rb') as f:
-    	print("this part works")
-    	print("======================")
+    # with open(web_classfier_file_path, 'rb') as f:
+    # 	print("this part works")
+    # 	print("======================")
     clf = None
     try:
-    	clf = pickle.load(open(web_classfier_file_path, 'rb'))
+    	clf = myfile # pickle.load(myfile) #open(web_classfier_file_path, 'rb'))
     except Exception as e:
-    	print(e.message)
+    	print(e)
+    	print("what the fuck ever")
     print("----------")
     return clf
     # return joblib.load(web_classfier_file_path)
