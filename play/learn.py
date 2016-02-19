96b42704a2837b5c9cb5752fd45dc2bcb3c444b9
oswald jones <oj@oswalds-MacBook-Pro-2.local>
2016-02-19 00:46:44 -0800

    trying something crazy

--- play/learn.py
+++ play/learn.py
@@ -1,28 +1,13 @@
-import pickle, json, os
+import pickle
+import urllib
 
+myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
+opener = urllib.URLopener()
+myfile = opener.open(myurl)
+clf = pickle.load(myfile)
 
 def get_saved_classifier():
-    myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
-    import urllib
-    opener = urllib.URLopener()
-    print(" ")
-    print("please work damnit")
-    print(" ")
-    myfile = opener.open(myurl)
-    clf = pickle.load(myfile)
     return clf
-
-def get_accuracy(choices):
-
-    classifier = get_classifier(choices)
-
-    metrics = {
-        'accuracy': classifier + ' accuracy'
-        , 'pos_fmeasure': classifier + ' pos_fmeasure'
-        , 'neg_fmeasure': classifier + ' neg_fmeasure'
-    }
-
-    return metrics
 
 def get_classifications(tweets):
     print('tweets')
