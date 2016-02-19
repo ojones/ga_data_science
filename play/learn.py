5aa39e0d736acf72e7dfd3c0564fb06fc13b4ee4
oswald jones <oj@oswalds-MacBook-Pro-2.local>
2016-02-18 23:19:08 -0800

    moved load into function

--- play/learn.py
+++ play/learn.py
@@ -1,31 +1,20 @@
 import pickle, json, os
 import urllib
-opener = urllib.URLopener()
-myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
-myfile = opener.open(myurl)
 # import urllib.request
 # with urllib.request.urlopen(myurl) as f:
 #     myfile = pickle.load(f)
-# from sklearn.externals import joblib
-
-# web_classfier_file_path = 'data/classifiers/me_uni_pp_100' 
-web_classfier_file_path = '/app/play/data/web_classifier.p'  
-web_classfier_file_path2 = 'data/web_classifier.p'  
 
 
 def get_saved_classifier():
-    # print(os.getcwd())
-    # os.chdir(os.path.dirname(__file__))
-    # print(os.getcwd())
-    # print("++++++++++")
-    # print("1" + str(os.path.isfile(web_classfier_file_path)))
-    # print("2" + str(os.path.isfile(web_classfier_file_path2)))
-    # with open(web_classfier_file_path, 'rb') as f:
-    # 	print("this part works")
-    # 	print("======================")
+	opener = urllib.URLopener()
+	myurl = "https://s3-us-west-2.amazonaws.com/ozclassifiers/web_classifier.p"
+	print(" ")
+	print("please work damnit")
+	print(" ")
+	myfile = opener.open(myurl)
     clf = None
     try:
-    	clf = myfile # pickle.load(myfile) #open(web_classfier_file_path, 'rb'))
+    	clf = myfile
     except Exception as e:
     	print(e)
     	print("what the fuck ever")
